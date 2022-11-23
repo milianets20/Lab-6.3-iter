520cfe615291d2cc2a7b42a6e8849f0452c3c7e7
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-24 01:21:01 +0200

    loth

---
+++ Lab6.3 inter test/Lab6.3 inter test.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.3 inter.cpp/Lab6.3 inter.cpp.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab63intertest
+{
+	TEST_CLASS(Lab63intertest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = 0;
+			int a[5] = { -1, 2, 0, 4, 8 };
+			t = find_maximum(a, 5);
+			Assert::AreEqual(t, 8);
+		}
+	};
+}
---
+++ Lab6.3 inter.cpp/Lab6.3 inter.cpp.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <ctime>
+
+using namespace std;
+
+void create(int* a, const int n, const int  HIGH, const int LOW)
+{
+   for (int i = 0; i < n; i++)
+       a[i] = LOW + rand() % (HIGH - LOW + 1);
+}
+void print(int* a, const int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+}
+int find_maximum(int* a, const int n)
+{
+    int max = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] >= max)
+           max = a[i];
+    }
+    return max;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+
+    const int n = 10;
+    int a[n];
+    const int HIGH = 53;
+    const int LOW = -17;
+
+    cout << "a[10] = { ";
+    create(a, n, HIGH, LOW);
+    print(a, n);
+    cout << "}" << endl;
+    cout << "F = " << find_maximum(a, n) << endl;
+
+    return 0;
+}
